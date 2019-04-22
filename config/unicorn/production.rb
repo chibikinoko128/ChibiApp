c769262b6eae9c330a83ee21b1cd5a9f09ff0990
Kazuhiro Hasebe <wonder.gate128@gmail.com>
2019-04-23 01:54:14 +0900

    first commit

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -26,15 +26,27 @@
 #fork前に行うことを定義。後述
   before_fork do |server, worker|
     defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
-    old_pid = "#{server.config[:pid]}.oldbin"
-    if old_pid != server.pid
+
+    old_pid = "#{ server.config[:pid] }.oldbin"
+    unless old_pid == server.pid
       begin
         sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
-        Process.kill "QUIT", File.read(old_pid).to_i
+        Process.kill :QUIT, File.read(old_pid).to_i
       rescue Errno::ENOENT, Errno::ESRCH
       end
     end
   end
+  #before_fork do |server, worker|
+    #defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
+    #old_pid = "#{server.config[:pid]}.oldbin"
+    #if old_pid != server.pid
+      #begin
+        #sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
+        #Process.kill "QUIT", File.read(old_pid).to_i
+      #rescue Errno::ENOENT, Errno::ESRCH
+      #end
+    #end
+  #end
 
 #fork後に行うことを定義。後述
   after_fork do |server, worker|
