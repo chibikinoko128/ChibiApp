122536665b61437c221ac3d38e8937b633e0d896
Kazuhiro Hasebe <wonder.gate128@gmail.com>
2019-05-21 01:24:08 +0900

    first commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,10 +94,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
-    #user_name: ENV["GMAIL_USERNAME"],
-    #password: ENV["GMAIL_PASSWORD"],
-    user_name: 'wonder.gate128@gmail.com',
-    password: 'lfhlzujjrmwmdccn',
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"],
     domain: 'smtp.gmail.com',
     authentication: 'plain',
     enable_starttls_auto: true
