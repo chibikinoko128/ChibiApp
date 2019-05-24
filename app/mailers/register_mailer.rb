75b51d808e2ace7295818a6913c7c1d2c0fac0e0
Kazuhiro Hasebe <wonder.gate128@gmail.com>
2019-05-25 05:59:48 +0900

    first commit

--- app/mailers/register_mailer.rb
+++ app/mailers/register_mailer.rb
@@ -1,5 +1,5 @@
 class RegisterMailer < ApplicationMailer
-  default from: 'ワンダーゲート運営局'<wonder.gate128@gmail.com>
+  default from: 'ワンダーゲート運営局'
   
   def payment(user)
     @time = Time.zone.now
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,6 +89,10 @@ Rails.application.configure do
     restrict_ip_addresses: false
   }
   
+  config.action_mailer.perform_deliveries = true
+  host = 'wonder-gate.com'
+  config.action_mailer.default_url_options = { host: host }
+  
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.smtp_settings = {
